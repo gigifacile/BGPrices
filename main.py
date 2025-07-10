41b125ee34e8c129b0f8c7594945cec4501be1ae
gigifacile <89297223+gigifacile@users.noreply.github.com>
2025-07-10 18:30:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -302,42 +302,6 @@ def process_url(game, url, scraper_func, fonte):
         print(f"[Errore {fonte}] {url} → {e}")
     return False
 
-def get_prezzi_gioco(nome_gioco, filename="PrezziAttuali.json"):
-    with open(filename, "r", encoding="utf-8") as f:
-        dati = json.load(f)
-
-    # Cerca il gioco nel JSON
-    for gioco in dati:
-        if gioco["name"].lower() == nome_gioco.lower():
-            prezzi = gioco.get("prezzi", {})
-            # Ordina per prezzo crescente e ritorna lista di tuple (store, prezzo, url)
-            return sorted(
-                [(store, info["price"], info["url"]) for store, info in prezzi.items()],
-                key=lambda x: x[1]
-            )
-    
-    # Se non trova il gioco
-    return None
-
-def get_storico_prezzi(nome_gioco: str) -> dict:
-    storico = defaultdict(list)
-
-    try:
-        with open("storico_prezzi.csv", newline="", encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                if row["gioco"].lower() == nome_gioco.lower():
-                    timestamp = row["timestamp"]
-                    store = row["store"]
-                    prezzo = row["prezzo"]
-                    storico[store].append((timestamp, prezzo))
-
-        return dict(storico)
-
-    except FileNotFoundError:
-        return {"errore": "File storico_prezzi.csv non trovato."}
-    except Exception as e:
-        return {"errore": str(e)}
 
 def main():
     with open(LISTA_PATH, "r", encoding="utf-8") as f:
